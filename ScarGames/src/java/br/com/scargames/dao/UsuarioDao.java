b1b04d70106b7dbb2e194ee6b9cae9417bb5b4e3
Dayane <dayanelopeslee@gmail.com>
2019-03-07 19:48:38 -0300

    Classes Dao criadas

--- ScarGames/src/java/br/com/scargames/dao/UsuarioDao.java
+++ ScarGames/src/java/br/com/scargames/dao/UsuarioDao.java
@@ -8,4 +8,76 @@ import org.hibernate.Session;
 
 public class UsuarioDao {
     
+    public List<Usuario> listar(){
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+        try{
+            List<Usuario> lista = session.createQuery("from Usuario order by nome asc").list();
+            session.getTransaction().commit();
+            return lista;
+        }catch(Exception e){
+            session.getTransaction().rollback();
+            e.printStackTrace();
+        }   
+        return new ArrayList();
+    }
+    
+    public Usuario consultar(Integer id){
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+        try{
+            Usuario produtora = (Usuario)session.createQuery("from Usuario where id = " + id).uniqueResult();
+            session.getTransaction().commit();
+            return produtora;
+        }catch(Exception e){
+            session.getTransaction().rollback();
+            e.printStackTrace();
+            return null;
+        }   
+    }
+    
+    public Boolean inserir (Usuario usuario){
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();  
+        session.beginTransaction();
+        try{
+            session.save(usuario);
+            session.getTransaction().commit();
+            return true;
+        }catch(Exception e){
+            session.getTransaction().rollback();
+                e.printStackTrace();
+            return false;
+        }       
+    }
+    
+    
+    public Boolean alterar (Usuario usuario){
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();  
+        session.beginTransaction();
+        try{
+            session.update(usuario);
+            session.getTransaction().commit();
+            return true;
+        }catch(Exception e){
+            session.getTransaction().rollback();
+                e.printStackTrace();
+            return false;
+        }      
+    }
+    
+    public Boolean excluir (Usuario usuario){
+       Session session = HibernateUtil.getSessionFactory().getCurrentSession();  
+        session.beginTransaction();
+        try{
+            session.delete(usuario);
+            session.getTransaction().commit();
+            return true;
+        }catch(Exception e){
+            session.getTransaction().rollback();
+                e.printStackTrace();
+            return false;
+        }      
+    }
+    
+   
 }
