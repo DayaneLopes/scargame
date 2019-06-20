25a41791a6e6616c14cb54ef3f3c69631b61a645
DayaneLopes <dayanelopeslee@gmail.com>
2019-06-19 23:59:34 -0300

    Classes UsuarioMB, EnderecoMB e Biblioteca criadas

--- ScarGames/src/java/br/com/scargames/cotroller/UsuarioMB.java
+++ ScarGames/src/java/br/com/scargames/cotroller/UsuarioMB.java
@@ -1,13 +1,10 @@
 package br.com.scargames.cotroller;
 
-import br.com.scargames.domain.Produtora;
 import br.com.scargames.domain.Usuario;
-import br.com.scargames.services.JogoService;
 import br.com.scargames.services.UsuarioService;
 import br.com.scargames.util.UtilMessages;
 import java.io.Serializable;
 import java.util.List;
-import javax.faces.bean.RequestScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -16,6 +13,10 @@ import javax.faces.bean.SessionScoped;
 public class UsuarioMB implements Serializable{
     
     private Usuario usuario;
+    private String nome;
+    private String cpf;
+    private String dataNascimento;
+    private String telefone;
     private String email;
     private String senha;
     private List<Usuario> usuarios;
@@ -28,6 +29,10 @@ public class UsuarioMB implements Serializable{
         UsuarioService service = new UsuarioService();
         usuarios = service.listar();
     } 
+    public String novo(){
+        usuario = new Usuario();
+        return "new.xhtml?faces-redirect=true";
+    }
     
     public void inicializarHibernate(){
         UsuarioService service = new UsuarioService();
@@ -42,6 +47,34 @@ public class UsuarioMB implements Serializable{
         }else{
             UtilMessages.messageError("Dados inválidos!");
             return null; 
+        }
+    }
+    
+    public String alterar(){
+        UsuarioService service = new UsuarioService();
+        if (service.alterar(usuario)){
+            UtilMessages.messageInfo("Usuario alterada com sucesso!");
+            this.listar();
+            return "list.xhtml?faces-redirect=true";
+        }else{
+            UtilMessages.messageError("Ocorreu um erro ao alterar usuario!");
+            return null;
+        }
+    }
+    public String carregarDados(Usuario usuario){
+        this.usuario = usuario;
+        return "alter.xhtml?faces-redirect=true";
+    }
+    
+    public String excluir(Usuario usuario){
+        UsuarioService service = new UsuarioService();
+        if (service.excluir(usuario)){
+            UtilMessages.messageInfo("Usuario excluída com sucesso!");
+            this.listar();
+            return "list.xhtml?faces-redirect=true";
+        }else{
+            UtilMessages.messageError("Erro ao excluir Usuario!");
+            return null;
         }
     }
 
@@ -76,6 +109,40 @@ public class UsuarioMB implements Serializable{
     public void setUsuarios(List<Usuario> usuarios) {
         this.usuarios = usuarios;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    
       
     
 }
